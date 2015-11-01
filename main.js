48b98bef217d013ce560e0999e5ae23c562e0b8e
Wenjun LIN <wenjun.lin@rea-group.com>
2015-11-01 20:53:06 +0800

    Add the cheat function

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 $(function() {
   var nameList = ['井寒', '董磊', '周奕星', '国莹', '王振璇', '孙亚楠', '孙恬静', '孙菁', '李亚', '李仕元', '赵少翌', '樊炜', '高阳', '李进', '杨楠晨', '陈曦宇', '李君丽', '王玥', '胡智', '王旭桐', '潘浩', '王绿菊', '罗杰', '翁娇娇', '姚丹萍', '李昱(♀)', '郭鑫', '闫文婷', '刘欣倩', '林杨', '李昱(♂)', '魏尧', '刘静'];
   var colorList = ['#019AA0', '#AFD693', '#FCE680', '#F2AE25', '#CF181D'];
+
+  var checkedIdx = -1;
+
   nameList.forEach(function(name, idx) {
     $('<li class="grid" />')
       .text(name)
@@ -9,11 +12,14 @@ $(function() {
 
       $('<li/>')
         .text(name)
-        .on('click', function() {
-          $(this).siblings('.checked').removeClass('checked');
-          $(this).addClass('checked')
-        })
+        .data('idx', idx)
         .appendTo($('#choose-box'));
+  })
+
+  $('#choose-box > li').on('click', function() {
+    $(this).siblings('.checked').removeClass('checked');
+    $(this).addClass('checked')
+    checkedIdx = ($(this).data('idx'));
   })
 
   var i=0;
@@ -24,16 +30,27 @@ $(function() {
   function rotate(degree) {
     $('.grid').each(function() {
       var absDeg = degree + $(this).index() * (360 / nameList.length) ;
-      $(this).css('transform', `rotateX(${absDeg}deg)`)
-        .css('transform', `rotateX(${absDeg}deg) translateZ(${translateZ}px)`);
+      $(this).css('transform', `rotateX(${absDeg}deg) translateZ(${translateZ}px)`);
     })
   }
-  var rndInt = 0;
-  rotate(rndInt);
+  var roundCount = 0;
+
+  rotate(roundCount);
+
   $('.grid').height();
   $(document).on('keydown', function(event) {
-    if(76 !== event.keyCode){return};
-    rndInt += parseInt(Math.random() * nameList.length) + 100;
-    rotate(rndInt * (360 / nameList.length));
+    var targetIdx;
+
+    if(76 !== event.keyCode && 88 !== event.keyCode){return};
+
+    if(76 === event.keyCode && parseInt(checkedIdx) !== -1) {
+      targetIdx = checkedIdx;
+    } else {
+      targetIdx = parseInt(Math.random() * nameList.length);
+    }
+
+    roundCount += parseInt(Math.random() * 3 + 3) * nameList.length;
+    var needRotete = (roundCount - targetIdx) * (360 / nameList.length);
+    rotate(needRotete);
   })
 })
