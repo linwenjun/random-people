55e31f080489687a1f88550842ce0511368c5b7c
Wenjun LIN <wenjun.lin@rea-group.com>
2015-11-02 22:38:23 +0800

    Add gulp-connect for project

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,25 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var buffer = require('vinyl-buffer');
 var minifyCss = require('gulp-minify-css');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: './',
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('./index.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./dist/scripts/dist.js'], ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
 
 gulp.task('browserify', function() {
   var b = browserify({
--- readme.md
+++ readme.md
@@ -4,3 +4,18 @@
 
 -	index.html为随机排队程序
 -	demo.html为随机抽学生程序
+
+#### todolist
+
+====
+
+-	gulp-connect
+-	gulp-watch
+-	整理gulp任务
+-	gulp task list
+-	拆分javascript
+-	less
+-	bootstrap样式修改
+-	抽离颜色到样式
+-	抽离数据
+-	增加文本框到数据
