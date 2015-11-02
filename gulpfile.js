07073cdefadd0d6e9c733d67ce886f86ebda89cf
Wenjun LIN <wenjun.lin@rea-group.com>
2015-11-02 21:47:15 +0800

    Use browserify and uglify to refine the scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,38 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
+var transform = require('vinyl-transform');
+var minifyCss = require('gulp-minify-css');
+var streamify = require('gulp-streamify')
+var rename = require('gulp-rename')
 
 gulp.task('browserify', function() {
-  return browserify('src/scripts/main.js')
-    .bundle()
+  var b = browserify({
+    entries: './src/scripts/main.js',
+    debug: true
+  });
+
+  return b.bundle()
     .pipe(source('dist.js'))
-    .pipe(gulp.dest('./dist/scripts'));
-})
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts/'));
+
+
+
+    // return browserify('./src/scripts/main.js')
+    //     .bundle()
+    //     .pipe(source('bundle.js'))
+    //     .pipe(uglify())
+    //     .pipe(gulp.dest('./dist/scripts/'));
+});
 
 gulp.task('css', function() {
   return gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css', 'src/css/main.css'])
     .pipe(concat('dist.css'))
+    .pipe(minifyCss())
     .pipe(gulp.dest('./dist/css'));
 })
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,12 @@
+'use strict';
+
 var $ = require('jquery');
 
-$(function() {
-  var nameList = ['井寒', '董磊', '周奕星', '国莹', '王振璇', '孙亚楠', '孙恬静', '孙菁', '李亚', '李仕元', '赵少翌', '樊炜', '高阳', '李进', '杨楠晨', '陈曦宇', '李君丽', '王玥', '胡智', '王旭桐', '潘浩', '王绿菊', '罗杰', '翁娇娇', '姚丹萍', '李昱(♀)', '郭鑫', '闫文婷', '刘欣倩', '林杨', '李昱(♂)', '魏尧', '刘静'];
-  var colorList = ['#019AA0', '#AFD693', '#FCE680', '#F2AE25', '#CF181D'];
+var nameList = ['井寒', '董磊', '周奕星', '国莹', '王振璇', '孙亚楠', '孙恬静', '孙菁', '李亚', '李仕元', '赵少翌', '樊炜', '高阳', '李进', '杨楠晨', '陈曦宇', '李君丽', '王玥', '胡智', '王旭桐', '潘浩', '王绿菊', '罗杰', '翁娇娇', '姚丹萍', '李昱(♀)', '郭鑫', '闫文婷', '刘欣倩', '林杨', '李昱(♂)', '魏尧', '刘静'];
+var colorList = ['#019AA0', '#AFD693', '#FCE680', '#F2AE25', '#CF181D'];
 
+
+$(function() {
   var checkedIdx = -1;
 
   nameList.forEach(function(name, idx) {
@@ -31,9 +34,9 @@ $(function() {
   $('.wrap').css('top', deletaTop);
   function rotate(degree) {
     $('.grid').each(function() {
-      var absDeg = degree + $(this).index() * (360 / nameList.length) ;
-      $(this).css('transform', `rotateX(${absDeg}deg) translateZ(${translateZ}px)`);
-    })
+      var absDeg = degree + $(this).index() * (360 / nameList.length);
+      $(this).css('transform', 'rotateX(' + absDeg + 'deg) translateZ(' + translateZ + 'px)');
+    });
   }
   var roundCount = 0;
 
@@ -54,5 +57,5 @@ $(function() {
     roundCount += parseInt(Math.random() * 3 + 3) * nameList.length;
     var needRotete = (roundCount - targetIdx) * (360 / nameList.length);
     rotate(needRotete);
-  })
-})
+  });
+});
